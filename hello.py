5475766dd84c47d77b65e72df7b097526032d9ae
jackey <jackey525@gmail.com>
2020-05-11 14:19:34 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,20 +1,24 @@
 from telegram.ext import Updater, CommandHandler
 from playsound import playsound
 
+# echo hello your name
 def hello(bot, update):
     update.message.reply_text(
         'hello, {}'.format(update.message.from_user.first_name))
     playsound('/Users/jackey/Downloads/song.mp3')
 
+# send a music to channel from local
 def play(bot, update):
     update.message.reply_text('play now')
     bot.send_audio(chat_id=1071195608, audio=open('/Users/jackey/Downloads/song.mp3', 'rb'))
     #playsound('/Users/jackey/Downloads/song.mp3')
 
+# send a document file to channel from local
 def file_handler(bot, update):
     bot.send_document(chat_id=1071195608, document=open('/Users/jackey/Downloads/GameClient_CDN_List.txt', 'rb'))
     update.message.reply_text('uploadload now')
-
+    
+# download file to local
 def download_handler(bot, update):    
     update.message.reply_text('start')
     msg = update.message
@@ -22,12 +26,15 @@ def download_handler(bot, update):
     file_id.download()
     update.message.reply_text('end')
 
-updater = Updater('1218300637:AAEjauTmz9JaJoodoD3l5Vtq91QoUKp1wr8')
+# write your token here
+updater = Updater('your token')
 
+# add_handler
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('playsound', play))
 updater.dispatcher.add_handler(CommandHandler('upload', file_handler))
 updater.dispatcher.add_handler(CommandHandler('qq', download_handler))
 
+# start_polling
 updater.start_polling()
 updater.idle()
